365593b12be9da2c98df4ee400efcff186964af7
Dwiki Ramdani <official.dwikiramdani@gmail.com>
2019-09-18 15:22:24 +0700

    Webview Implemented

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -50,24 +50,15 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        child: Container(
+          color: Colors.cyan,
+          width: double.infinity,
+          child: WebView(
+            initialUrl: 'http://jakarta-tourism.go.id',
+            javascriptMode: JavascriptMode.unrestricted,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
